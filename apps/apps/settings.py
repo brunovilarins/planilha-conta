0227aa978371f5ec4ccab978ad8c531900083041
brunovilarins <tigaruco>
2019-03-07 18:20:17 -0300

    Configuração dos arquivos Static.

--- apps/apps/settings.py
+++ apps/apps/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
+rel = lambda p: os.path.join(PROJECT_ROOT, p)
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,7 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = rel('static/')
+
 STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 
 REST_SESSION_LOGIN = True
